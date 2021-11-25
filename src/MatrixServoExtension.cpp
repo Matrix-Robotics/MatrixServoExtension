9977e5f48d5f251594295d4f3d5c03064fe9ea87
DCisHurt <46733725+DCisHurt@users.noreply.github.com>
2021-11-25 13:54:29 +0800

    update MuxSelect delay time

--- src/MatrixServoExtension.cpp
+++ src/MatrixServoExtension.cpp
@@ -114,14 +114,14 @@ void MatrixServo::i2cMUXSelect(){
 		Wire.write(_ch + 4);
 		Wire.endTransmission(1);
 
-		delay(10);
+		delayMicroseconds(300);
 		break;
 	case 3:
 		Wire.beginTransmission(ADDR_PCA954X);
 		Wire.write((1 << _ch));
 		Wire.endTransmission(1);
 		
-		delay(10);
+		delayMicroseconds(300);
 		break;
 	default:
 		break;
